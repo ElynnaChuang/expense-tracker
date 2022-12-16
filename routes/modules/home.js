10dda8cee7d2fa970e58d39db0a33e812021c183
yichennn <yichennn1010@gmail.com>
2022-12-16 23:28:48 +0800

    頁面：總金額動態改變

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -9,18 +9,18 @@ router.get('/', (req, res) => {
     .lean()
     .sort({ date: 1 })
     .then(records => {
-      return Promise.all(Array.from(
-        { length: records.length },
-        (_, i) => {
-        const { categoryID } = records[i]
-        return Category.findById( categoryID )
-          .then(category => records[i].category = { name: category.name, icon: category.icon })
-      }))
-      .then(() => res.render('index', { records }))
-      .catch(err => {
-        console.log(err)
-        res.render('error', { err })
-      })
+      let totalAmount = 0
+      return Promise.all(Array.from({ length: records.length }, (_, i) => {
+          const { categoryID } = records[i]
+          totalAmount += records[i].amount
+          return Category.findById( categoryID )
+            .then(category => records[i].category = { name: category.name, icon: category.icon })
+        }))
+        .then(() => res.render('index', { records, totalAmount }))
+        .catch(err => {
+          console.log(err)
+          res.render('error', { err })
+        })
     })
     .catch(err => {
       console.log(err)
