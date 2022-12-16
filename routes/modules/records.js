67453310cfaf6acb29699b126e51911bac438121
yichennn <yichennn1010@gmail.com>
2022-12-16 23:14:16 +0800

    功能：新增支出

--- routes/modules/records.js
+++ routes/modules/records.js
@@ -13,25 +13,47 @@ const isSelected = require('../../utilities/isSelected')
 
 //---新增---//
 router.get('/new', (req, res) => {
-  res.render('new')
+  res.render('new', { categories })
 })
 
 router.post('/', (req, res) => {
-
+  const { name, date, amount, category } = req.body
+  Category.findOne({ name: category })
+    .then((category) => {
+      const categoryID = category._id
+      Record.create({ name, date, amount, categoryID})
+        .then(() => res.redirect('/'))
+        .catch(err => {
+          console.log(err)
+          res.render('error', { err })
+        })
+    })
+    .catch(err => {
+      console.log(err)
+      res.render('error', { err })
+    })
 })
 
 //---修改---//
 router.get('/:id/edit', (req, res) => {
   const _id = req.params.id
   Record.findOne({ _id })
-  .lean()
-  .then(record => {
-    const { categoryID } = record
-    Category.findById(categoryID)
-      .then(category => {
+    .lean()
+    .then(record => {
+      const { categoryID } = record
+      Category.findById(categoryID)
+        .then(category => {
           const optionHTML = isSelected(categories, category.name)
           res.render('edit', { record, optionHTML })
         })
+        .catch(err => {
+          console.log(err)
+          res.render('error', { err })
+        })
+    })
+    .catch(err => {
+      console.log(err)
+      res.render('error', { err })
     })
 })
 
