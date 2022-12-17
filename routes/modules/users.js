d9f8bf24793298d9b00c8e5a676edd74b951c1da
yichennn <yichennn1010@gmail.com>
2022-12-17 22:46:14 +0800

    資料：新增User model 頁面：登入＆註冊 靜態頁面

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,7 +3,7 @@ const router = express.Router()
 
 //---登入---//
 router.get('/login', (req, res) => {
-
+  res.render('login')
 })
 
 router.post('/login', (req, res) => {
@@ -17,7 +17,7 @@ router.post('/logout', (req, res) => {
 
 //---註冊---//
 router.get('/register', (req, res) => {
-
+  res.render('register')
 })
 
 router.post('/register', (req, res) => {
