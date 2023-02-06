733e3b1e9e8153ecbe8e3e8b99b62122becf89e1
yichennn <yichennn1010@gmail.com>
2023-02-06 18:16:22 +0800

    調整種子資料邏輯＆file name

--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -8,8 +8,8 @@ const db = require('../../config/mongoose')
 
 const bcrypt = require('bcryptjs')
 const SEEDER_USER = {
-  name: 'test',
-  email: 'test@test.com',
+  name: 'user1',
+  email: 'user1@example.com',
   password: '12345678'
 }
 
@@ -18,31 +18,33 @@ db.once('open', () => {
     .genSalt(10)
     .then(salt => bcrypt.hash(SEEDER_USER.password, salt))
     .then(hash => {
-      // ============ 產生test使用者 ============ //
+      // ============ 產生 User ============ //
       const { name, email } = SEEDER_USER
-      User.create({ name, email, password: hash})
-        .then( user => {
-          const userID = user._id
-          Category.find()
-            .lean()
-            .then(categories => {
-              return Promise.all(Array.from({ length: 10 },
-              (_, i) => {
-                const index = i%(categories.length) //0~4循環
-                return Record.create({
-                  name: `name-${i+1}`,
-                  date: `2022-12-15`,
-                  amount: (i+1)*10,
-                  categoryID: categories[index]._id,
-                  userID
-                })
-              }))
+      return User.create({ name, email, password: hash})
+      .catch(err => console.log('create User error', err))
+    })
+    .then( user => {
+      const userID = user._id
+      return Category.find().lean()
+        .then(categories => {
+          return Promise.all(Array.from({ length: 10 }, (_, i) => {
+            const index = i%(categories.length) //0~4循環
+            // ============ 產生 Record ============ //
+            return Record.create({
+              name: `name-${i+1}`,
+              date: `2022-12-15`,
+              amount: (i+1)*10,
+              categoryID: categories[index]._id,
+              userID
             })
-            .then(() => {
-              console.log('recordSeeder done');
-              process.exit()
-            })
+            .catch(err => console.log('create Record error', err))
+          }))
         })
+        .catch(err => console.log('find Category error', err))
+    })
+    .then(() => {
+      console.log('All done!');
+      process.exit()
     })
     .catch(err => console.log('create recordSeeder error', err))
 })
